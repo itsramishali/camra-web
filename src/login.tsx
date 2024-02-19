7c2e33b50c18a34a12e408fd3fb67e17a689fd7c
itsramishali <112942711+itsramishali@users.noreply.github.com>
2024-02-19 20:17:14 +0500

    change..

---
+++ src/login.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword, registerWithEmailAndPassword } from './auth';
+import './login.css'; // Import the CSS file
+
+const LoginPage: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            setError('');
+            await signInWithEmailAndPassword(email, password);
+            // Handle successful login
+        } catch (error) {
+            setError((error as Error).message);
+            // Handle login error
+        }
+    };
+
+    // const handleRegister = async () => {
+    //     try {
+    //         setError('');
+    //         await registerWithEmailAndPassword(email, password); // Removed the second argument
+
+    //         // If registration is successful, handle it accordingly
+    //     } catch (error) {
+    //         setError((error as Error).message);
+    //         // Handle registration error
+    //     }
+    // };
+    const handleRegister = async () => {
+        try {
+            setError('');
+            await registerWithEmailAndPassword(email,); // Pass email and password as separate arguments
+    
+            // If registration is successful, handle it accordingly
+        } catch (error) {
+            setError((error as Error).message);
+            // Handle registration error
+        }
+    };
+    
+    return (
+        <div>
+            <header className="header">
+                <img className='logo-img' src="/logo.png" alt="" />
+                <h1 className='head-ing'>CamRa</h1>
+            </header>
+            <div className="login-container">
+                <form className="login-form">
+                    <label htmlFor="email">Username/Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+
+                    <button type="button" onClick={handleLogin}>Login</button>
+                    <button type="button" onClick={handleRegister}>Register</button>
+                </form>
+
+                {error && <p className="error-message">{error}</p>}
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
+
--- src/videomeet.tsx
+++ src/videomeet.tsx
@@ -7,13 +7,13 @@ const App = () => {
   const rtcProps = {
     appId: '5dff16dd1f7b424ab1a92a130e9d3176',
     channel: 'mychannel', // your agora channel
-    token: '007eJxTYNhadSx3/9K1avMiZBdziEyouMnC7ph67HwBR2ieUMOl0kIFBtOUtDRDs5QUwzTzJBMjk8Qkw0RLo0RDY4NUyxRjQ3Mz6bDLqQ2BjAyebVwsjAwQCOJzMuRWJmck5uWl5jAwAABulR80' // use null or skip if using app in testing mode
+    token: '007eJxTYODaIFHjFHqs/nO8X+6fRQUaD2++vxq3l7Ex6YnAp9yTyhoKDKYpaWmGZikphmnmSSZGJolJhomWRomGxgaplinGhuZmNQWXUxsCGRlk9TQYGRkgEMTnZMitTM5IzMtLzWFgAAAlQCGA' // use null or skip if using app in testing mode
   };
   const callbacks = {
     EndCall: () => setVideoCall(false),
   };
   return videoCall ? (
-    <div style={{ margin: "0", padding:"0", display: 'flex', width: '90vw', height: '98vh'}}>
+    <div style={{  display: 'flex', width: '90vw', height: '98vh'}}>
       <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
     </div>
   ) : (
